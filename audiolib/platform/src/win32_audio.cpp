796932a41f3c23a45b29374b2b05951c7ddab032
Jonathan <joh2001@protonmail.com>
2021-11-08 21:32:32 +0100

    °˖✧◝(⁰▿⁰)◜✧˖° My initial commit. Let's gooo °˖✧◝(⁰▿⁰)◜✧˖°

---
+++ audiolib/platform/src/win32_audio.cpp
@@ -0,0 +1,5 @@
+#include <Windows.h>
+
+void test() {
+    OutputDebugStringW(L"Moin!");
+}
---
+++ audiolib/platform/src/win32_audio.h
@@ -0,0 +1,11 @@
+//
+// Created by Jonathan on 08.11.2021.
+//
+
+#ifndef AUDIOFY_LIB_WIN32_AUDIO_H
+#define AUDIOFY_LIB_WIN32_AUDIO_H
+#ifndef WIN32_LEAN_AND_MEAN
+    #define WIN32_LEAN_AND_MEAN
+#endif
+
+#endif //AUDIOFY_LIB_WIN32_AUDIO_H
