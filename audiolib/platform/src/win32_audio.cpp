a9fffa3030327c6d19024e921d2868545ad7907b
Jonathan <joh2001@protonmail.com>
2021-11-08 23:45:00 +0100

    Added functions to retrieve (default) win32 audio endpoints, added skeleton for handling win32 endpoint devices changes, added avx memory stuff (we might need that for dsp later maybe)

--- audiolib/platform/src/win32_audio.cpp
+++ audiolib/platform/src/win32_audio.cpp
@@ -1,5 +1,145 @@
 #include <Windows.h>
+#include "win32_audio.h"
+#include "memUtil.h"
 
-void test() {
-    OutputDebugStringW(L"Moin!");
+namespace PlatformWin32 {
+    constexpr u32 INVALID_PROP_DESCRIPTOR = 2;
+
+    u32 getDescFromEndpoint(_In_ IMMDevice* endpoint,
+                            _Out_ AudioDevice* _device) {
+        using Microsoft::WRL::ComPtr;
+        HRESULT result;
+        LPWSTR id = nullptr;
+        result = endpoint->GetId(&id);
+        if(FAILED(result) || id == nullptr) {return 0;}
+        _device->deviceID = id;
+
+        ComPtr<IPropertyStore> props;
+        result = endpoint->OpenPropertyStore(STGM_READ, props.GetAddressOf());
+        if(FAILED(result) || props.GetAddressOf() == nullptr) {return 0;}
+        PROPVARIANT var;
+        PropVariantInit(&var);
+        result = props->GetValue(PKEY_Device_FriendlyName, &var);
+        if(FAILED(result)) {return 0;}
+
+        _device->endpoint = endpoint;
+        _device->var = var;
+
+        if(var.vt != VT_LPWSTR) {
+            _device->description = nullptr;
+            return INVALID_PROP_DESCRIPTOR;
+        }
+
+        _device->description = var.pwszVal;
+        DWORD state;
+        result = endpoint->GetState(&state);
+        if(FAILED(result)) {return 0;}
+        _device->currentState = state;
+
+        return 1;
+    }
+
+    u32 getAvailableAudioOutputDevices(_Out_ AudioDeviceList* _devices,
+                                       _Out_ u32* numDevices) {
+        using Microsoft::WRL::ComPtr;
+        ComPtr<IMMDeviceEnumerator> devEnum;
+        ComPtr<IMMDeviceCollection> devices;
+
+        HRESULT result = CoCreateInstance(__uuidof(MMDeviceEnumerator),
+                                          nullptr,
+                                          CLSCTX_INPROC_SERVER,
+                                          IID_PPV_ARGS(devEnum.GetAddressOf()));
+        if(FAILED(result)) {
+            goto exit;
+        }
+
+        result = devEnum->EnumAudioEndpoints(eRender,
+                                             DEVICE_STATE_ACTIVE,
+                                             &devices);
+        if(FAILED(result)) {goto exit;}
+
+        u32 deviceCount = 0;
+        result = devices->GetCount(&deviceCount);
+        if(FAILED(result) || deviceCount == 0) {goto exit;}
+
+        *numDevices = deviceCount;
+        auto* deviceList = new AudioDevice[deviceCount];
+
+        for(u32 i = 0; i < deviceCount; ++i) {
+            ComPtr<IMMDevice> endpoint;
+            result = devices->Item(i, endpoint.GetAddressOf());
+            if(FAILED(result) || endpoint.GetAddressOf() == nullptr) {goto exit;}
+
+            auto res = getDescFromEndpoint(endpoint.Get(), &deviceList[i]);
+            if(!res) {goto exit;}
+            if(res == INVALID_PROP_DESCRIPTOR) {continue;}
+        }
+
+        _devices->deviceCount = deviceCount;
+        _devices->devices = deviceList;
+
+        return 1;
+        exit:
+        errno = result;
+        return 0;
+    }
+
+    u32 getDefaultAudioOutputDevice(_Out_ AudioDevice* device) {
+        using Microsoft::WRL::ComPtr;
+
+        ComPtr<IMMDeviceEnumerator> devEnum;
+        HRESULT result = CoCreateInstance(__uuidof(MMDeviceEnumerator),
+                                          nullptr,
+                                          CLSCTX_INPROC_SERVER,
+                                          IID_PPV_ARGS(devEnum.GetAddressOf()));
+
+        ComPtr<IMMDevice> endpoint;
+
+        if(FAILED(result)) {
+            goto exit;
+        }
+
+        result = devEnum->GetDefaultAudioEndpoint(eRender, eMultimedia,
+                                                  endpoint.GetAddressOf());
+        if(FAILED(result)) {
+            goto exit;
+        }
+        AudioDevice defaultDevice {0};
+        if(!getDescFromEndpoint(endpoint.Get(),&defaultDevice)) {goto exit;}
+        return 1;
+        exit:
+        {
+            errno = result;
+            return 0;
+        };
+
+    }
+    void freeAudioDevice(_In_ AudioDevice *device) {
+        CoTaskMemFree(device->deviceID);
+        device->deviceID = nullptr;
+        PropVariantClear(&device->var);
+        device->description = nullptr;
+        device->endpoint->Release();
+        device->endpoint = nullptr;
+    }
+
+    HRESULT AudioDeviceNotifiactionHandler::OnDefaultDeviceChanged(EDataFlow flow, ERole role, LPCWSTR pwstrDeviceID) {
+        //TODO: Handle this
+        return S_OK;
+    }
+
+    HRESULT AudioDeviceNotifiactionHandler::OnDeviceAdded(LPCWSTR pwstrDeviceId) {
+        //TODO: Handle this
+        return S_OK;
+    }
+
+    HRESULT AudioDeviceNotifiactionHandler::OnDeviceStateChanged(LPCWSTR pwstrDeviceId, DWORD dwNewState) {
+        //TODO: Handle this
+        return S_OK
+    }
+
+    HRESULT AudioDeviceNotifiactionHandler::OnPropertyValueChanged() {
+        //TODO: Handle this
+        return S_OK
+    }
 }
--- audiolib/platform/src/win32_audio.h
+++ audiolib/platform/src/win32_audio.h
@@ -7,5 +7,92 @@
 #ifndef WIN32_LEAN_AND_MEAN
     #define WIN32_LEAN_AND_MEAN
 #endif
+#include "types.h"
+#include <mfidl.h>
+#include <mmdeviceapi.h>
+#include <functiondiscoverykeys_devpkey.h>
+#include <wrl/client.h>
+#include <xaudio2.h>
+namespace PlatformWin32
+{
+    struct AudioDevice {
+        wchar_t* deviceID;
+        wchar_t* description;
+        tagPROPVARIANT var;
+
+        IMMDevice* endpoint;
+        DWORD currentState;
+    };
+
+    struct AudioDeviceList {
+        u32 deviceCount;
+        AudioDevice* devices;
+    };
+
+
+#define SAFE_RELEASE(punk)  \
+    if ((punk) != NULL)  \
+    { (punk)->Release(); (punk) = NULL; }
+
+    class AudioDeviceNotifiactionHandler : public IMMNotificationClient {
+        LONG _cRef;
+        IMMDeviceEnumerator *_enumerator;
+
+    public:
+        AudioDeviceNotifiactionHandler() : _cRef(1), _enumerator(nullptr) {}
+        ~AudioDeviceNotifiactionHandler() { SAFE_RELEASE(_enumerator)}
+
+        ULONG STDMETHODCALLTYPE AddRef() {
+            return InterlockedIncrement(&_cRef);
+        }
+        ULONG STDMETHODCALLTYPE Release() {
+            ULONG ulRef = InterlockedDecrement(&_cRef);
+            if(!ulRef) {
+                delete this;
+            }
+            return ulRef;
+        }
+        HRESULT STDMETHODCALLTYPE QueryInterface(REFIID riid, void **ppvInterface) {
+            if(IID_IUnknown == riid) {
+                AddRef();
+                *ppvInterface = (IUnknown*)this;
+            } else if(__uuidof(IMMNotificationClient) == riid) {
+                AddRef();
+                *ppvInterface = (IMMNotificationClient*)this;
+            } else {
+                *ppvInterface = nullptr;
+                return E_NOINTERFACE;
+            }
+            return S_OK;
+        }
+        HRESULT STDMETHODCALLTYPE OnDefaultDeviceChanged(EDataFlow flow, ERole role, LPCWSTR pwstrDeviceID) override;
+        HRESULT STDMETHODCALLTYPE OnDeviceAdded(LPCWSTR pwstrDeviceId) override;
+        HRESULT STDMETHODCALLTYPE OnDeviceStateChanged(LPCWSTR pwstrDeviceId, DWORD dwNewState);
+        HRESULT STDMETHODCALLTYPE OnPropertyValueChanged();
+    };
+
+    /// Retrieves all audio output devices currently available
+    /// \param devices OUT: Pointer to all available devices, free with freeAudioDeviceList
+    /// \param numDevices OUT: Pointer to the number of devices found
+    /// \return Non-Zero on success
+    u32 getAvailableAudioOutputDevices(_Out_ AudioDeviceList* devices,
+    _Out_ u32* numDevices);
+
+    u32 getDefaultAudioOutputDevice(_Out_ AudioDevice* device);
+
+    /// Frees an audio device
+    void freeAudioDevice(_In_ AudioDevice* device);
+
+    void freeAudioDeviceList(_In_ AudioDeviceList* list) {
+        for(int i = 0; i<list->deviceCount; ++i) {
+            freeAudioDevice(&list->devices[i]);
+        }
+        delete[] list->devices;
+        list->deviceCount = 0;
+        list->devices = nullptr;
+    }
+
+}
+
 
 #endif //AUDIOFY_LIB_WIN32_AUDIO_H
