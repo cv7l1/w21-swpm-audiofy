0430bc1937db397248a21b67abc38e4c519f7799
marioblm <47089917+marioblm@users.noreply.github.com>
2021-12-09 18:13:31 +0100

    Add GUI

---
+++ gui/build_win32.bat
@@ -0,0 +1,8 @@
+@REM Build for Visual Studio compiler. Run your copy of vcvars32.bat or vcvarsall.bat to setup command-line compiler.
+@set OUT_DIR=Debug
+@set OUT_EXE=example_glfw_opengl3
+@set INCLUDES=/Iimgui /Iimgui\backends /Iimgui\libs\glfw\include
+@set SOURCES=src/main.cpp imgui\backends\imgui_impl_glfw.cpp imgui\backends\imgui_impl_opengl3.cpp imgui\imgui*.cpp
+@set LIBS=/LIBPATH:imgui\libs\glfw\lib-vc2010-32 glfw3.lib opengl32.lib gdi32.lib shell32.lib
+mkdir %OUT_DIR%
+cl /nologo /Zi /MD %INCLUDES% %SOURCES% /Fe%OUT_DIR%/%OUT_EXE%.exe /Fo%OUT_DIR%/ /link %LIBS%
