470b20fa2b05cb1b2f28f0e001d7254dde2aa4dd
MaGrabert <92442019+MaGrabert@users.noreply.github.com>
2022-01-06 15:00:39 +0100

    Split Components in seperate Files
    #16

---
+++ gui/src/components/plot.h
@@ -0,0 +1,9 @@
+#include <math.h>
+#include <array>
+void showPlot() {
+	ImGui::SetNextWindowPos(ImVec2(0, 0));
+	ImGui::SetNextWindowSize(ImVec2(905, 664));
+	ImGui::Begin("Plot");
+	
+	ImGui::End();
+}
