7a4b9ae546c6dbd389c4870a32f25057073e1a45
marioblm <47089917+marioblm@users.noreply.github.com>
2022-01-08 16:26:23 +0100

    Add different color schemes to GUI

--- gui/src/components/toolbar.h
+++ gui/src/components/toolbar.h
@@ -1,4 +1,4 @@
-void showToolBar() {
+void showToolBar(ImVec4* colors) {
     if (ImGui::BeginMainMenuBar())
     {
         if (ImGui::BeginMenu("Files"))
@@ -27,6 +27,35 @@ void showToolBar() {
             else if (ImGui::MenuItem("Leveling"))
             {
                 //Reset Leveling
+            }
+            ImGui::EndMenu();
+        }
+
+        if (ImGui::BeginMenu("Color Schemes"))
+        {
+            if (ImGui::MenuItem("White"))
+            {
+                colors[ImGuiCol_WindowBg] = ImVec4(0.39f, 0.00f, 0.63f, 0.11f);
+                colors[ImGuiCol_TitleBg] = ImVec4(0.39f, 0.00f, 0.63f, 0.11f);
+                colors[ImGuiCol_MenuBarBg] = ImVec4(0.39f, 0.00f, 0.63f, 0.11f);
+                colors[ImGuiCol_PopupBg] = ImVec4(0.39f, 0.00f, 0.63f, 0.11f);
+                colors[ImGuiCol_Text] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+            }
+            else if (ImGui::MenuItem("Black"))
+            {
+                colors[ImGuiCol_WindowBg] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+                colors[ImGuiCol_TitleBg] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+                colors[ImGuiCol_MenuBarBg] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+                colors[ImGuiCol_PopupBg] = ImVec4(0.0f, 0.0f, 0.0f, 1.0f);
+                colors[ImGuiCol_Text] = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
+            }
+            else if (ImGui::MenuItem("Blue"))
+            {
+                colors[ImGuiCol_WindowBg] = ImVec4(0.050f, 0.0f, 0.521f, 1.0f);
+                colors[ImGuiCol_TitleBg] = ImVec4(0.050f, 0.0f, 0.521f, 1.0f);
+                colors[ImGuiCol_MenuBarBg] = ImVec4(0.050f, 0.0f, 0.521f, 1.0f);
+                colors[ImGuiCol_PopupBg] = ImVec4(0.050f, 0.0f, 0.521f, 1.0f);
+                colors[ImGuiCol_Text] = ImVec4(1.0f, 1.0f, 1.0f, 1.0f);
             }
             ImGui::EndMenu();
         }
